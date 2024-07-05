a2a31bb8db65975a40248edf652445a31510c3a2
Adarsh N Adakane <121501048+adarshadakane@users.noreply.github.com>
2024-07-05 11:12:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -36,11 +36,13 @@ variable "acl" {
     description = " Defaults to private "
     default     = "private"
 }
+
 variable "bucket_prefix" {
     type        = string
     description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
     default     = "my-s3bucket-"
 }
+
 variable "tags" {
     type        = map
     description = "(Optional) A mapping of tags to assign to the bucket."
